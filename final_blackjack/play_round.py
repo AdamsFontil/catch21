56ea3c9bc1131313c1d935cc89d2406c6c7a245d
adams <108461931+AdamsFontil@users.noreply.github.com>
2026-09-13 15:51:33 -0400

    modified game logic to simplify things

--- final_blackjack/play_round.py
+++ final_blackjack/play_round.py
@@ -4,31 +4,75 @@ from deal_cards import deal_card, reset_deck
 
 def game_logic(player,house,bet):
   print('hello game logic')
-  blackjack = play_first_round(player,house, bet)
-  while blackjack != True :
-    # if player.hand[0] > 21:
-    #   print('player lost')
-    #   player.withdraw(bet)
-    #   break
+  for i in range(4):
+    if i % 2 == 0:
+      draw_card(house)
+    else:
+      draw_card(player)
+
+  print('c hand', house.hand)
+  print('c hand2', house.hand[:1])
+  print('p hand', player.hand)
+
+  p_hand_val = player.hand_val(player.hand)
+  c_hand_val = house.hand_val(house.hand[:1])
+
+  for i in range(15):
+    p_hand_val = player.hand_val(player.hand)
+    if p_hand_val[0] == 21 or p_hand_val[1] == 21:
+      print('p hand is', p_hand_val)
+      print('player has won!!!')
+      break
+    elif p_hand_val[0] != p_hand_val[1] and p_hand_val[1] <= 21:
+      print(f'1computer has {c_hand_val} and you have {p_hand_val[0]} or {p_hand_val[1]}')
+    elif p_hand_val[0] > 21:
+      print('p hand is', p_hand_val)
+      print('player has lost')
+      break
+    else:
+      print(f'2computer has {c_hand_val} and you have {p_hand_val[0]}')
+      print('p hand is', p_hand_val)
+
     choice = str(input('please type to (h)it, (s)tay or (q)uit to continue : '))
     match choice:
       case 'h':
-        hand = p_turn(player, house)
-        if hand[0] > 21:
-          print('lost', hand)
-          player.withdraw(bet)
-          break
-        elif hand[0] == 21 or hand[1] == 21:
-          print('win', hand)
-          player.deposit(bet)
-          break
+        draw_card(player)
       case 's':
         print('s')
       case 'q':
-        print('going back to main menu')
+        print('quit')
         break
-      case _:
-        print('error: please type h or s to continue the game or q to quit game')
+
+
+
+
+  # while blackjack != True :
+  #   # if player.hand[0] > 21:
+  #   #   print('player lost')
+  #   #   player.withdraw(bet)
+  #   #   break
+  #   choice = str(input('please type to (h)it, (s)tay or (q)uit to continue : '))
+  #   match choice:
+  #     case 'h':
+  #       hand = p_turn(player, house)
+  #       if hand[0] > 21:
+  #         print('lost', hand)
+  #         player.withdraw(bet)
+  #         break
+  #       elif hand[0] == 21 or hand[1] == 21:
+  #         print('win', hand)
+  #         player.deposit(bet)
+  #         break
+  #     case 's':
+  #       while True:
+  #         outcome = c_turn(player, house, bet)
+  #         if outcome == 'p_lost' or outcome == 'p_win':
+  #           break
+  #     case 'q':
+  #       print('going back to main menu')
+  #       break
+  #     case _:
+  #       print('error: please type h or s to continue the game or q to quit game')
 
 player = Player('adams', 250)
 house = Players('computer')
@@ -69,54 +113,37 @@ def p_turn(player, house):
       print(f'The computer has {c_hand_val[0]} and you have {hand_val[0]} or {hand_val[1]}')
       return hand_val
 
+def c_turn(player, house,bet):
+    c_draw = deal_card()
+    house.hand.append(c_draw)
+    c_hand_val = house.hand_val(house.hand)
+    hand_val = player.hand_val(player.hand)
+    print(f'c_hand val', c_hand_val)
+    print('c hand', house.hand)
+    if c_hand_val[0] == c_hand_val[1] or c_hand_val[1] > 21:
+      print(f'The computer has {c_hand_val[0]} and you have {hand_val[0]}')
+    if c_hand_val[0] == 21 or c_hand_val[1] == 21:
+      print('computer has blackjack, you lost')
+      player.withdraw(bet)
+      return 'p_lost'
+    if c_hand_val[0] > 21:
+      print('computer has busted, you won')
+      player.deposit(bet)
+      return 'p_win'
+    else:
+      print(f'you have {hand_val[0]} and computer has {c_hand_val[0]} or {c_hand_val[1]}')
+
+
+def draw_card(player):
+    card = deal_card()
+    player.hand.append(card)
+
+
+
+
 
 game_logic(player, house, 100)
 
-# Function game_menu:
-# prompt the user asking them what would they like to do next
-# if they respond with ‘h’ or ‘hit’
-# call_deal card for the player
-# print you now have {player.hand_val}
-# if player.hand_val > 21:
-# print player lost
-# reduces the player’s bank by the bet amount
-# reset player.hand and house.hands to [ ]
-# reset cards_dealt to [ ]
-# set game_over to true
-# else if player.hand_val = 21:
-# print player won
-# increases the player’s bank by the bet amount
-# reset player.hand and house.hands to [ ]
-# reset cards_dealt to [ ]
-# set game_over to true
-# else:
-# call game_menu
 
-# else if they respond with ‘s’ or ‘stay’
-# call_deal card for the house
-# if house.hand_val > 21:
-# print player won
-# increases the player’s bank by the bet amount
-# reset player.hand and house.hands to [ ]
-# reset cards_dealt to [ ]
-# set game_over to true
-# else if house.hand_val = 21:
-# print player lost
-# decreases the player’s bank by the bet amount
-# reset player.hand and house.hands to [ ]
-# reset cards_dealt to [ ]
-# set game_over to true
-# else:
-# call game_menu
-# else:
-# tell user to choose (‘hit or stay’)
 
-# Function play_round()
-# while game_over is false
-# for 4 rounds:
-# if the round is odd:
-# call the deal cards function and add the card to the player.hand
-# else:
-# call the deal cards function and add the card to the house.hand
-# print player has this value, and computer has one val, where one card is hidden
-# call the game_menu function
+#play round, takes player returns hand_val?
